df267c7025b86c3ad8001b564193161de7ed0d6b
Alifbrz <54773837+Alifbrz@users.noreply.github.com>
2023-07-10 15:27:29 +0700

    Update settings.py

--- flowery_days/settings.py
+++ flowery_days/settings.py
@@ -200,13 +200,6 @@ if 'USE_AWS' in os.environ:
 
 
 # Stripe
-FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PERCENTAGE = 10
-STRIPE_CURRENCY = 'eur'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-
 # Email Settings
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
